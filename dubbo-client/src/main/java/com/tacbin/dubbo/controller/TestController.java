cd7f66f97e670108232e2e81b74baaad6d21f0e4
drinktea <null>
2019-11-05 09:29:51 +0800

    dubbo远程调用

--- dubbo-client/src/main/java/com/tacbin/dubbo/controller/TestController.java
+++ dubbo-client/src/main/java/com/tacbin/dubbo/controller/TestController.java
@@ -1,10 +1,16 @@
 package com.tacbin.dubbo.controller;
 
-import com.alibaba.dubbo.config.annotation.Reference;
-import com.tacbin.dubbo.serviceimp.RemoteService;
+import com.alibaba.dubbo.common.extension.ExtensionLoader;
+import com.sun.xml.internal.bind.v2.model.core.ID;
+import com.tacbin.dubbo.spi.IDubboInsertHelloWorld;
+import com.tacbin.dubbo.spi.IDubboSpiService;
+import com.tacbin.dubbo.spi.ISpiService;
+import com.tacbin.dubbo.spi.impl.RemoteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ServiceLoader;
 
 /**
  * @Description :
@@ -19,5 +25,27 @@ public class TestController {
     @RequestMapping("/")
     public String remoteService() {
         return service.helloWorld();
+    }
+
+    @RequestMapping("/spi")
+    public String spiTest() {
+        ServiceLoader<ISpiService> spiServices = ServiceLoader.load(ISpiService.class);
+        String value = "";
+        for (ISpiService service : spiServices) {
+            value += service.spiTest();
+        }
+        return value;
+    }
+
+    @RequestMapping("/dubboSpi")
+    public String dubboSpi() {
+        IDubboSpiService spiService = ExtensionLoader.getExtensionLoader(IDubboSpiService.class).getExtension("impl");
+        return spiService.spiTest();
+    }
+
+    @RequestMapping("/autoWrapper")
+    public String autoWrapper(){
+        IDubboInsertHelloWorld helloWorld = ExtensionLoader.getExtensionLoader(IDubboInsertHelloWorld.class).getExtension("impl");
+        return helloWorld.getHelloWorld();
     }
 }
--- dubbo-client/src/main/java/com/tacbin/dubbo/service/IMyService.java
+++
@@ -1,6 +0,0 @@
-package com.tacbin.dubbo.service;
-
-public interface IMyService {
-    String helloWorld();
-}
-
