c74602e326eda6252028d9b6c99043a4c6d5c052
drinktea <null>
2019-11-20 13:15:37 +0800

    修改dubbo版本 先午睡了

--- dubbo-client/src/main/java/com/tacbin/dubbo/spi/impl/LocalService.java
+++ dubbo-client/src/main/java/com/tacbin/dubbo/spi/impl/LocalService.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Service;
  **/
 @Service
 public class LocalService {
-    @Reference
+    @Reference(check = false)
     ILocalProvider localProvider;
 
     public String helloWorld() {
